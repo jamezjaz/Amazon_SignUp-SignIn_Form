3660e3a6eb7263407ec22a9a74645b0a85752aa7
Jamezjaz <jamezjaz@gmail.com>
2021-07-11 11:18:24 +0100

    docs: Implements handleChange feature

--- src/container/Register.js
+++ src/container/Register.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import RegisterStyles from '../styles/RegisterStyles.module.css';
 
 const Register = () => { // eslint-disable-line
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirm_password: '',
+  });
+
+  const handleNameChange = event => {
+    setState({
+      ...state,
+      name: event.target.value,
+    });
+  };
+
+  const handleEmailChange = event => {
+    setState({
+      ...state,
+      email: event.target.value,
+    });
+  };
+
+  const handlePasswordChange = event => {
+    setState({
+      ...state,
+      password: event.target.value,
+    });
+  };
+
+  const handleConfirmPasswordChange = event => {
+    setState({
+      ...state,
+      confirm_password: event.target.value,
+    });
+  };
+
   return (
     <div className={RegisterStyles.container}>
       <form className={RegisterStyles.form}>
@@ -14,7 +49,8 @@ const Register = () => { // eslint-disable-line
             id="name"
             name="name"
             type="text"
-            placeholder="Your Name"
+            value={state.name}
+            onChange={handleNameChange}
           />
         </label>
         <label htmlFor="email">
@@ -24,7 +60,8 @@ const Register = () => { // eslint-disable-line
             id="email"
             name="email"
             type="text"
-            placeholder="Your Email"
+            value={state.email}
+            onChange={handleEmailChange}
           />
         </label>
         <label htmlFor="password">
@@ -35,6 +72,8 @@ const Register = () => { // eslint-disable-line
             name="password"
             type="password"
             placeholder="At least 6 characters"
+            value={state.password}
+            onChange={handlePasswordChange}
           />
         </label>
         <small>Passwords must be at least 6 characters.</small>
@@ -45,6 +84,8 @@ const Register = () => { // eslint-disable-line
             id="conf-password"
             name="conf-password"
             type="password"
+            value={state.confirm_password}
+            onChange={handleConfirmPasswordChange}
           />
         </label>
         <div>
@@ -62,7 +103,7 @@ const Register = () => { // eslint-disable-line
           <Link to="/sign-in">Sign-in</Link>
         </div>
       </form>
-      <div>
+      <div className="mb-4">
         <Link to="/">Conditions of Use  Privacy Notice  Help</Link>
         <br />
         <small>© 1996-2021, Amazon.com, Inc. or its affiliates</small>
